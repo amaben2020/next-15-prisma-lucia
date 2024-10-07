91fa134a99a212bcd73dd7418399d79459a841df
amaben2020 <amaben202phantom@gmail.com>
2024-10-07 14:59:03 +0100

    progressive enhancement

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -18,7 +18,8 @@ const SearchField = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    // progressive enhancement: ensuring featutr works without js
+    <form onSubmit={handleSubmit} method="GET" action="/search">
       <div className="relative">
         <Input name="q" placeholder="Search" className="pe-10" />
         <SearchIcon className="absolute right-3 top-1/2 size-5 -translate-y-1/2 transform text-muted-foreground" />
