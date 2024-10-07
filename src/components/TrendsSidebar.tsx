009522c0b21da39204182adb200a86200591df9f
amaben2020 <amaben202phantom@gmail.com>
2024-10-07 20:16:13 +0100

    trending logic

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -6,6 +6,8 @@ import Link from 'next/link';
 import React, { Suspense } from 'react';
 import UserAvatar from './UserAvatar';
 import { Button } from './ui/button';
+import { unstable_cache } from 'next/cache';
+import { formatNumber } from '@/lib/utils';
 
 const TrendsSidebar = () => {
   return (
@@ -13,6 +15,7 @@ const TrendsSidebar = () => {
       {/* suspense only works in child components */}
       <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
         <WhoToFollow />
+        <TrendingTopics />
       </Suspense>
     </div>
   );
@@ -60,6 +63,48 @@ async function WhoToFollow() {
           <Button>Follow</Button>
         </div>
       ))}
+    </div>
+  );
+}
+
+const getTrendingTopics = unstable_cache(
+  async () => {
+    const result = await prisma.$queryRaw<{ hashtag: string; count: bigint }[]>`
+  SELECT LOWER(unnest(regexp_matches(content, '#[[:alnum:]_]+', 'g'))) AS hashtag, COUNT(*) AS count FROM posts GROUP BY (hashtag) ORDER BY count DESC, hashtag ASC limit 5
+  `;
+
+    return result.map((row) => ({
+      hashtag: row.hashtag,
+      count: Number(row.count),
+    }));
+  },
+  ['trending_topics'],
+  {
+    revalidate: 3 * 60 * 60,
+  }
+);
+
+async function TrendingTopics() {
+  const trendingTopics = await getTrendingTopics();
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="text-xl font-bold">
+        {trendingTopics.map(({ hashtag, count }) => {
+          const title = hashtag.split('#')[1];
+
+          return (
+            <Link key={title} href={`/hashtag/${title}`} className="block">
+              <p className="line-clamp-1 break-all font-semibold hover:underline">
+                {hashtag}
+              </p>
+
+              <p className="text-sm text-muted-foreground">
+                {formatNumber(count)} {count === 1 ? 'post' : 'posts'}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
