ea6b405e85897fae9bb181a582a20dcbdcddc00b
amaben2020 <amaben202phantom@gmail.com>
2024-10-07 19:01:32 +0100

    adding tiptap

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,14 +1,12 @@
+import PostEditor from '@/components/posts/editor/PostEditor';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="h-[200vh] w-full bg-red-500">
       {' '}
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-        reiciendis saepe, assumenda doloremque minima aspernatur quidem,
-        provident veniam perferendis error fugiat non iusto! Dolorum, accusamus.
-        Quae officiis ab blanditiis tenetur.
+      <div className="w-full">
+        <PostEditor />
       </div>
     </main>
   );
---
+++ src/components/posts/editor/PostEditor.tsx
@@ -0,0 +1,44 @@
+'use client';
+import { EditorContent, useEditor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import Placeholder from '@tiptap/extension-placeholder';
+import { submitPost } from './actions';
+import { useSession } from '@/app/(main)/SessionProvider';
+import UserAvatar from '@/components/UserAvatar';
+const PostEditor = () => {
+  const { user } = useSession();
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        bold: false,
+        italic: false,
+      }),
+      Placeholder.configure({
+        placeholder: 'Whats crack-a-lacking?',
+      }),
+    ],
+  });
+  const input =
+    editor?.getText({
+      blockSeparator: '\n',
+    }) || '';
+
+  async function onSubmit() {
+    await submitPost(input);
+    editor?.commands.clearContent();
+  }
+
+  return (
+    <div className="flex flex-col gap-5 rounded-2xl bg-card p-5 shadow">
+      <div className="flex gap-5">
+        <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
+        <EditorContent
+          editor={editor}
+          className="w-full max-h-[20rem] overflow-y-auto bg-background rounded-2xl px-5 py-3"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PostEditor;
